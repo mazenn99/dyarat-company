daf4b3b7b0094038c4fa2dd62c46a6f794335faa
Mr.SToRM <mazenn99@gmail.com>
2021-06-27 14:11:12 +0300

    change photo

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,7 +8,7 @@
             <div class="row">
                 <div class="col-lg-6 d-flex flex-column justify-content-center">
                     <h1 data-aos="fade-up">{{__('index.company_name')}}</h1>
-                    <h2 data-aos="fade-up" data-aos-delay="400">{{__('index.short_description')}}</h2>
+{{--                    <h2 data-aos="fade-up" data-aos-delay="400">{{__('index.short_description')}}</h2>--}}
                     <div data-aos="fade-up" data-aos-delay="600">
                         <div class="text-center text-lg-start">
                             <a href="#about"
