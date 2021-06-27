35dc8f1f436fac8e81a231293969fc012f7b0fde
Mr.SToRM <mazenn99@gmail.com>
2021-06-27 14:04:11 +0300

    change photo index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -20,7 +20,7 @@
                     </div>
                 </div>
                 <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
-                    <img src="{{asset('assets/img/features.png')}}" class="img-fluid" alt="">
+                    <img src="{{asset('assets/img/img_site.jpg')}}" class="img-fluid" alt="">
                 </div>
             </div>
         </div>
